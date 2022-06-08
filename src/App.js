0a1d2da624d352d0aa4fce49fa07ef083b10a088
durbonca <durbonca@gmail.com>
2022-06-08 15:35:52 -0400

    update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
     <Routes>
       <Route path="/qr_for_userwise_reports/v2/MTk4MTI0/4bc40e6b-ea1d-11eb-9a89-02c5f122bffd/Mjk5NDM0NTA=/" element={<Document usuario="manuel" />} />
       <Route path="/qr_for_userwise_reports/v2/NiIsInR5cCI6/aWNfa2V-JjNDB-WViL-9a89-02c5f122bffd/ZCI6NTUzMTc=/" element={<Document usuario="iracema" />} />
+      <Route path="/qr_for_userwise_reports/v2/NiIsInR5cCI6/aWNfa2V-JjNDB-WViL-9a89-02c5f122bffd/THSD45dfsDD=/" element={<Document usuario="jose" />} />
+      <Route path="/qr_for_userwise_reports/v2/NiIsInR5cCI6/aWNfa2V-JjNDB-WViL-9a89-02c5f122bffd/Mjkdf5NDM0NTA=/" element={<Document usuario="maria" />} />
       <Route path="/" element={<Component />} />
     </Routes>
     </div>
--- src/Document.jsx
+++ src/Document.jsx
@@ -2,6 +2,9 @@ import React, {useEffect} from 'react'
 import _document from './docs/_descarga.pdf'
 import document from './docs/descarga.pdf'
 
+import _document_ from './docs/_descarga_.pdf'
+import document_ from './docs/descarga_.pdf'
+
 const Component = ({ usuario }) => {
 
     useEffect(() => {
@@ -10,6 +13,12 @@ const Component = ({ usuario }) => {
         }
         if (usuario === 'iracema'){
             window.location.href = _document;
+        }
+        if (usuario === 'jose'){
+            window.location.href = _document_;
+        }
+        if (usuario === 'maria'){
+            window.location.href = document_;
         }
     })
 
